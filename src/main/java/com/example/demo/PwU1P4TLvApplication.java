9f590bf4a91de64ef988000ff7adaf1680d9ae59
levalladares <levalladares@uce.edu.ec>
2023-05-13 21:37:12 -0500

    deber 3 commit 1

---
+++ src/main/java/com/example/demo/PwU1P4TLvApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TLvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TLvApplication.class, args);
+	}
+
+}
